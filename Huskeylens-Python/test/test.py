f3a5a07867e3d2cde037f895e5f8094284831e53
Tomoni-Code-Academy <tomonidevelopers@gmail.com>
2024-12-14 20:46:25 +0900

    Update test.py

--- Huskeylens-Python/test/test.py
+++ Huskeylens-Python/test/test.py
@@ -1,3 +1,21 @@
 #
-# Test code for Huskylens driver (runs on LEGO SPIKE HUB)
+# Test code for Huskylens driver (runs on ESP32-C3)
 #
+import machine
+from huskylens_lib import HuskyLens
+
+uart1 = machine.UART(1)
+uart1.init(baudrate=9600, bits=8, parity=None, stop=1, tx=21, rx=20)
+
+husky = HuskyLens(uart1)
+#husky.send_CMD_REQ_ALGO('FACE_RECOG')
+husky.send_CMD_REQ_ALGO('COLOR_RECOG')
+#husky.send_CMD_REQ_ALGO('TAG_RECOG')
+#husky.send_CMD_REQ_ALGO('LINE_TRACK')
+#husky.send_CMD_REQ_ALGO('TAG_RECOG')
+
+while True:
+   print(husky.read_block())
+
+
+
